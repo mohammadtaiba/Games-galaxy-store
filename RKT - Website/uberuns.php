cb2398643aea38f4aa592a27fbd07f85ea977972
Falko Kühn <fa2476ku@fhe-ad.fh-erfurt.de>
2023-11-27 13:50:42 +0100

    Created Includes Restructured Project Static Navbar finished Responsive Navbar started

---
+++ RKT - Website/uberuns.php
@@ -0,0 +1,13 @@
+<?php
+$title = "Über Uns";
+require_once('includes/head.php');
+require_once('includes/navbar.php');
+?>
+<body>
+<div>
+    Hier ist der Body
+</div>
+</body>
+<footer>
+    Hier ist der Footer
+</footer>
