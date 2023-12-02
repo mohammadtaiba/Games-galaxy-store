9ff51dc3bd18e87e4dfdd38119b10120ab06c6f6
Pionec <dennis.rinck@fh-erfurt.de>
2023-12-02 12:16:55 +0100

    included footer for all sites

--- RKT - Website/battlenetspiele.php
+++ RKT - Website/battlenetspiele.php
@@ -3,3 +3,6 @@ $title = "Battle.net";
 require_once('includes/head.php');
 require_once('includes/navbar.php');
 ?>
+<?php
+require_once('includes/footer.php');
+?>
--- RKT - Website/startseite.php
+++ RKT - Website/startseite.php
@@ -7,29 +7,29 @@ require_once('includes/navbar.php');
 <div class="StartseiteGrids">
     <div class="grid-kategorie">Angebote</div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-left fa-xl"></i></button></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Diablo4-logo.jpg" class="game-images"  alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Diablo4-logo.jpg" class="game-images"  alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-right fa-xl"></i></button></div>
 </div>
 <div class="StartseiteGrids">
     <div class="grid-kategorie">Bestseller</div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-left fa-xl"></i></button></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Diablo4-logo.jpg" class="game-images" alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Diablo4-logo.jpg" class="game-images" alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-right fa-xl"></i></button></div>
 </div>
 <div class="StartseiteGrids">
     <div class="grid-kategorie">Trends</div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-left fa-xl"></i></button></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Diablo4-logo.jpg" class="game-images" alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
-    <div class="grid-element"><a class="game-images" href="eldenring.php"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/elden-ring-logo.jpg" class="game-images" alt="Cover für das erste Spiel"></a><div class="grid-element-background">Elden Ring</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Diablo4-logo.jpg" class="game-images" alt="Cover für das zweite Spiel"></a><div class="grid-element-background">Diablo 4</div></div>
+    <div class="grid-element"><a class="game-images" href="/"><img src="Images/Satisfactory-cover.jpg" class="game-images" alt="Cover für das dritte Spiel"></a><div class="grid-element-background">Satisfactory</div></div>
     <div class="anglediv"><button class="angle"><i class="fa-solid fa-angle-right fa-xl"></i></button></div>
 </div>
 </body>
 <?php
 require_once('includes/footer.php');
 ?>
-<script src="index.js"></script>
+
