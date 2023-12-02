ac45a8f6adb2b922d1a0b3e69dc196fc8cfda988
Pionec <dennis.rinck@fh-erfurt.de>
2023-12-02 13:07:36 +0100

    created footer mobile

--- RKT - Website/includes/footer.php
+++ RKT - Website/includes/footer.php
@@ -1,10 +1,13 @@
 <?php
 echo <<< FOOTER
 <footer>
+
     <div id="footer-brand">Games Galaxy &reg</div>
-            <div class="footer-element"><a href="/" class="footer-links">Kontaktier uns </a></div>
-            <div class="footer-element"><a href="/" class="footer-links">Impressum</a></div>
-            <div class="footer-element"><a href="/" class="footer-links">FAQ</a></div>
+    <div class="footer-desktop"><a href="/" class="footer-links">Kontaktier uns </a></div>
+    <div class="footer-desktop"><a href="/" class="footer-links">Impressum</a></div>
+    <div class="footer-desktop"><a href="/" class="footer-links">FAQ</a></div>
+
+    <div id="footer-impressum"><a href="/" class="footer-links">Impressum</a></div>
 </footer>
 
 <script src="index.js"></script>
