4f2b479d24049de076f8aae251d7c23fb0efa52c
Falko Kühn <fa2476ku@fhe-ad.fh-erfurt.de>
2023-12-03 12:08:30 +0100

    created most of needed files linked sites together added few comments

---
+++ RKT - Website/profilbearbeiten.php
@@ -0,0 +1,11 @@
+<?php
+$title = "Profil bearbeiten";
+require_once('includes/head.php');
+require_once('includes/navbar.php');
+?>
+<body>
+
+</body>
+<?php
+require_once('includes/footer.php');
+?>
