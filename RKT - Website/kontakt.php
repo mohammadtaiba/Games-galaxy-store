57d3686ad3960adbf59f16cd2fff9aac53db00b1
Falko Kühn <fa2476ku@fhe-ad.fh-erfurt.de>
2023-12-08 21:11:52 +0100

    created static content for kontakt.php und impressum.php

--- RKT - Website/kontakt.php
+++ RKT - Website/kontakt.php
@@ -4,7 +4,24 @@ require_once('includes/head.php');
 require_once('includes/navbar.php');
 ?>
 <body>
-
+<div class="statisch">
+<h1>Kontakt</h1>
+    <div class="statischinhalt">
+        <div class="kontaktelemente">
+            <h3>Dennis Rinck</h3>
+            <p>dennis.rinck@fh-erfurt.de</p>
+            <h3>Mohammad Taiba</h3>
+            <p>mohammad.taiba@fh-erfurt.de</p>
+            <h3>Falko Kühn</h3>
+            <p>falko.kuehn@fh-erfurt.de</p>
+            <h3>Games Galaxy</h3>
+            <p>0815 187187420</p>
+            <p>Musterstraße 1312</p>
+            <p>99084 Erfurt</p>
+            <p>games.galaxy@email.com</p>
+        </div>
+    </div>
+</div>
 </body>
 <?php
 require_once('includes/footer.php');
