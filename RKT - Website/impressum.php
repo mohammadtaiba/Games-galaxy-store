21cf1d30e40f22e05733609b6a5f5b13e5c9da74
Falko Kühn <fa2476ku@fhe-ad.fh-erfurt.de>
2023-12-10 00:54:14 +0100

    added skript to connect webserver to database (database close right after running the skript, need to fix somehow) added skript to disconnect webserver and database as soon as bug is fixed

--- RKT - Website/impressum.php
+++ RKT - Website/impressum.php
@@ -19,6 +19,8 @@ require_once('includes/navbar.php');
             <p>Falko Kühn (CEO)</p>
             <div id="dokumentation">
             <a href="https://onedrive.live.com/?authkey=%21ANM5jF6w%5FLJs2Vg&id=8B04937D20B87665%2149190&cid=8B04937D20B87665"><h3>Dokumentation ></h3></a>
+                <a href="database.php">Datenbankverbindung öffnen</a><br>
+                <a href="close_connection.php">Datenbankverbindung schließen</a>
             </div>
         </div>
     </div>
---
+++ RKT - Website/includes/login.php
@@ -0,0 +1,8 @@
+<?php
+
+$host = 'localhost';
+$data = 'gg_dbms';
+$user = 'ggadmin';
+$pass = 'ggpassword';
+$chrs = 'utf8mb4';
+$attr = "mysql:host=$host;dbname=$data;charset=$chrs";
