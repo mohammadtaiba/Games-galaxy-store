70aee28dee7328342b09aa48de2d08537f76379b
Falko Kühn <fa2476ku@fhe-ad.fh-erfurt.de>
2023-12-20 10:28:24 +0100

    created model structure and few needed models for existing controllers, changed sql to reflect on changes

---
+++ gamesgalaxy/lib/DatabaseConnection.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace gamesgalaxy\lib\DatabaseConnection;
+
+use mysqli;
+
+class DatabaseConnection
+{
+
+    private static \mysqli $instance;
+
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+
+    public static function get_instance(): \mysqli
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new mysqli("localhost", "ggadmin", "ggpassword", "gg_dbms");
+        }
+
+        return self::$instance;
+    }
+
+}
---
+++ gamesgalaxy/model/BearbeitenModel.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace gamesgalaxy\Model;
+
+require_once __DIR__."/../model/Model.php";
+require_once __DIR__."/../lib/DatabaseConnection.php";
+
+use gamesgalaxy\lib\DatabaseConnection\DatabaseConnection;
+
+class BearbeitenModel extends Model
+{
+    private \mysqli $db;
+
+    public function __construct()
+    {
+        $this->db = DatabaseConnection::get_instance();
+    }
+
+    function read()
+    {
+
+    }
+
+    function create()
+    {
+
+    }
+
+    function update()
+    {
+
+    }
+
+    function delete()
+    {
+
+    }
+
+    function read_all()
+    {
+
+    }
+
+    function match_and_read()
+    {
+
+    }
+
+    function delete_all()
+    {
+
+    }
+}
