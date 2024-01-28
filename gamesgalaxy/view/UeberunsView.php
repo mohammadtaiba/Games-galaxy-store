86c2d76cac224b78c0d3e8931e9c94f562b89902
mohammad <mohammad.taiba@fh-erfurt.de>
2024-01-28 16:38:21 +0100

    Ueberuns-Bugs korrigiert.

--- gamesgalaxy/view/UeberunsView.php
+++ gamesgalaxy/view/UeberunsView.php
@@ -20,20 +20,25 @@ class UeberunsView extends View
                             Egal, ob Sie ein erfahrener Spieler sind oder gerade erst Ihre Gaming-Reise beginnen, Games Galaxy ist ihre Anlaufstelle für Videspiele.<br>
                             Erkunden Sie mit uns das Universum der Gaming-Möglichkeiten, und lassen Sie das Abenteuer beginnen!</p>
                     </div>
-                    <div id="uberunsliste">
-                        <h2>Warum Sie Games Galaxy nutzen sollten:</h2>
-                        <p><span class="bulletpoints">&bull; Große Auswahl:</span> Entdecken Sie eine umfangreiche und vielfältige Sammlung von Videspielen die auf jede Spielpräferenz und Plattform auf dem PC zugeschnitten ist.</p>
-                        <p><span class="bulletpoints">&bull; Neueste Veröffentlichungen:</span> Bleiben Sie mit Zugang zu den neusten und heißesten Spielveröffentlichungen an vorderster Front des Gamings, um sicherzustellen, dass Sie nie die neuesten Trends und Innovationen verpassen.</p>
-                        <p><span class="bulletpoints">&bull; Wettbewerbsfähige Preise:</span> Genießen Sie wettbewerbsfähige und erschwingliche Preise für alle unsere Spiele, sodass Spiele für jeden zugänglich sind, ohne das Budget zu strapazieren.</p>
-                        <p><span class="bulletpoints">&bull; Benutzerfreundliche Oberfläche:</span> Navigieren Sie mühelos durch unsere benutzerfreundliche Website und genießen Sie ein reibungsloses und angenehmes Einkaufserlebnis vom Stöbern bis zum Checkout.</p>
-                        <p><span class="bulletpoints">&bull; Schneller Versand:</span> Erhalten Sie ihre Spiele schnell und zuverlässig per Mail in Form eines Produkt-Codes, welchen Sie bei der Platform ihrer Wahl einlösen können, um sofort in Ihre neuen Spiele eintauchen zu können, ohne unnötige Verzögerunen.</p>
-                    </div>
-                    <div id="neuregistrieren">
-                        <h2>Sie besitzen noch keinen Account?</h2>
-                        <a id="ueber_uns_link" href="/dwp_ws2324_rkt/gamesgalaxy/Registrieren/Show"><div class="ueber_uns_button">Jetzt Registrieren!</div></a>
-                    </div>
-                </div>
+	                    <div id="uberunsliste">
+	                        <h2>Warum Sie Games Galaxy nutzen sollten:</h2>
+	                        <p><span class="bulletpoints">&bull; Große Auswahl:</span> Entdecken Sie eine umfangreiche und vielfältige Sammlung von Videspielen die auf jede Spielpräferenz und Plattform auf dem PC zugeschnitten ist.</p>
+	                        <p><span class="bulletpoints">&bull; Neueste Veröffentlichungen:</span> Bleiben Sie mit Zugang zu den neusten und heißesten Spielveröffentlichungen an vorderster Front des Gamings, um sicherzustellen, dass Sie nie die neuesten Trends und Innovationen verpassen.</p>
+	                        <p><span class="bulletpoints">&bull; Wettbewerbsfähige Preise:</span> Genießen Sie wettbewerbsfähige und erschwingliche Preise für alle unsere Spiele, sodass Spiele für jeden zugänglich sind, ohne das Budget zu strapazieren.</p>
+	                        <p><span class="bulletpoints">&bull; Benutzerfreundliche Oberfläche:</span> Navigieren Sie mühelos durch unsere benutzerfreundliche Website und genießen Sie ein reibungsloses und angenehmes Einkaufserlebnis vom Stöbern bis zum Checkout.</p>
+	                        <p><span class="bulletpoints">&bull; Schneller Versand:</span> Erhalten Sie ihre Spiele schnell und zuverlässig per Mail in Form eines Produkt-Codes, welchen Sie bei der Platform ihrer Wahl einlösen können, um sofort in Ihre neuen Spiele eintauchen zu können, ohne unnötige Verzögerunen.</p>
+	                    </div>
+           			</div>
             </div>
 UEBERUNS;
+	    if (!isset($_SESSION['user_authenticated']) && $_SESSION['user_authenticated'])
+	    {
+		    echo '<div id="neuregistrieren">';
+            echo '    <h2 id="neuregistrieren_h2">Sie besitzen noch keinen Account?</h2>';
+            echo '    <a id="ueber_uns_link" href="/dwp_ws2324_rkt/gamesgalaxy/Registrieren/Show"><div class="ueber_uns_button">Jetzt Registrieren!</div></a>';
+            echo '</div>';
+	    }
+
+
     }
 }
